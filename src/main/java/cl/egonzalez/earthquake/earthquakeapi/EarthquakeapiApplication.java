50c3bc038ac196f9985afd12db9dd9208e22536e
Edgar González <edgar.gonzalez@taisachile.cl>
2019-11-21 20:50:59 -0300

    linea base

---
+++ src/main/java/cl/egonzalez/earthquake/earthquakeapi/EarthquakeapiApplication.java
@@ -0,0 +1,13 @@
+package cl.egonzalez.earthquake.earthquakeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthquakeapiApplication.class, args);
+	}
+
+}
