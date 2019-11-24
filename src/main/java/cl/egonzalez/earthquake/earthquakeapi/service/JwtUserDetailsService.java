b3fa3956fcb45fad2bf7d2bec62739b7f8959abf
Edgar González <edgar.gonzalez@taisachile.cl>
2019-11-24 00:36:41 -0300

    se agrega JWt

---
+++ src/main/java/cl/egonzalez/earthquake/earthquakeapi/service/JwtUserDetailsService.java
@@ -0,0 +1,19 @@
+package cl.egonzalez.earthquake.earthquakeapi.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) {
+		// solo para efectos de prueba "password"
+		return new User(username, "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6", new ArrayList<>());
+	}
+
+}
